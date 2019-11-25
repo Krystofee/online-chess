ef9043f5e42cd0e36a4cb729816673c961b2735d
Krystof Rehacek <krystofee@gmail.com>
2019-11-25 21:49:26 +0100

    implemented Knight moves

--- src/stores/pieces/Knight.ts
+++ src/stores/pieces/Knight.ts
@@ -1,8 +1,28 @@
+import { computed } from 'mobx';
 import BasePiece from './BasePiece';
+import { generateOffsetMoves } from '../helpers';
 
 class Knight extends BasePiece implements IPiece {
   constructor(gameStore: IChessGameStore, color: PieceColor, position: Coord) {
     super(gameStore, 'N', color, position);
+  }
+
+  @computed get possibleMoves(): Move[] {
+    return generateOffsetMoves(
+      this,
+      this.game.pieces,
+      [
+        { x: 1, y: 2 },
+        { x: -1, y: 2 },
+        { x: 2, y: 1 },
+        { x: -2, y: 1 },
+        { x: 1, y: -2 },
+        { x: -1, y: -2 },
+        { x: 2, y: -1 },
+        { x: -2, y: -1 },
+      ],
+      false,
+    );
   }
 }
 
