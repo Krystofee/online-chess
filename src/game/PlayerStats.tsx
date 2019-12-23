4049890daa6eb5f8ad6979d42422c30ed78d5bfe
Krystof Rehacek <krystofee@gmail.com>
2019-12-23 12:22:27 +0100

    added online dot

--- src/game/PlayerStats.tsx
+++ src/game/PlayerStats.tsx
@@ -11,11 +11,14 @@ type Props = {
 const PlayerStats = ({ game, player }: Props) => {
   return (
     <Flexbox justifyContent="space-between" alignItems="center" style={{ height: '3rem', color: 'white' }}>
-      <div>
+      <Flexbox alignItems="center">
         <div style={{ padding: '0.75rem 0.5rem', backgroundColor: '#ffffff10' }}>
           <span style={{ fontSize: '1.5rem' }}>{renderTime(player.remainingTime)}</span>
         </div>
-      </div>
+        <div style={{ padding: '0.75rem 0.5rem', color: player.state === 'CONNECTED' ? '#3c9d3c' : '#5d5d5d' }}>
+          <span className="circle" />
+        </div>
+      </Flexbox>
       <div>
         <Flexbox alignItems="center" style={{ padding: '0.75rem 0.5rem' }}>
           <span style={{ fontSize: '1.3rem', letterSpacing: '0.2rem' }}>♞♜</span>
